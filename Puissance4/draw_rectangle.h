d5a970400c5110b2b3012802488d39795afcb3ba
theblueshadow3 <53496752+tbs3@users.noreply.github.com>
2021-10-11 10:39:13 +0200

    Add files via upload

---
+++ Puissance4/draw_rectangle.h
@@ -0,0 +1,8 @@
+#include <Xgeii.h>
+#ifndef DRAW_RECTANGLE_H
+#define DRAW_RECTANGLE_H
+
+void draw_rectangle(window window, unsigned int pos_x, unsigned int pos_y, unsigned int width, unsigned int height);
+void draw_rectangle_full(window window, unsigned int pos_x, unsigned int pos_y, unsigned int width, unsigned int height);
+
+#endif // DRAW_RECTANGLE_H
